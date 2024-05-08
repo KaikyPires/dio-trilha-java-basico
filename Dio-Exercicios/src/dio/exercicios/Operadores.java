1316c1fb8c43bc13e63fe929527149ae1ff0c3fd
Kaiky Pires <kaiky.pires@yahoo.com.br>
2024-05-08 18:59:32 -0300

    Teste de commit

---
+++ Dio-Exercicios/src/dio/exercicios/Operadores.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dio.exercicios;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        //ARITIMETICOS
+        int a = 10;         int b = 5;
+        int soma = a + b;       // Soma
+        int subtracao = a - b;  // Subtração
+        int multiplicacao = a * b;  // Multiplicação
+        int divisao = a / b;    // Divisão
+        int resto = a % b;      // Resto da divisão
+        //Operadores de Atribuição:
+        int c = 10;
+        c += 5;  // Equivalente a: c = c + 5;
+        c -= 3;  // Equivalente a: c = c - 3;
+        c *= 2;  // Equivalente a: c = c * 2;
+        c /= 4;  // Equivalente a: c = c / 4;
+        c %= 3;  // Equivalente a: c = c % 3;
+        //Operadores de Comparação:
+        int x = 10;          int y = 5;
+        boolean igual = x == y;   // Igual a
+        boolean diferente = x != y;  // Diferente de
+        boolean maiorQue = x > y;   // Maior que
+        boolean menorQue = x < y;   // Menor que
+        boolean maiorOuIgual = x >= y;  // Maior ou igual a
+        boolean menorOuIgual = x <= y;  // Menor ou igual a
+        //Logicos
+        boolean p = true;    boolean q = false;
+        if(p && q){// E lógico
+        }
+        boolean ouLogico = p || q;  // Ou lógico
+        boolean nao = !p;           // Negação lógica/ vai inverter
+        //Incremento ou desencremento
+        int num = 5;
+        num++;   // Incremento (equivalente a num = num + 1)
+        num--;   // Decremento (equivalente a num = num - 1)
+
+
+
+    }
+    
+}
---
+++ Dio-Exercicios/src/dio/exercicios/Variaveis_Constantes.java
@@ -0,0 +1,21 @@
+
+package dio.exercicios;
+
+public class Variaveis_Constantes {
+
+    public static void main(String[] args) {
+        //VARIAVEIS
+        int idade = 25;
+        double preco = 19.99;
+        boolean estaChovendo = true;
+        char letra = 'A';
+        String nome = "João";
+        long populacao = 10000000000L;//tem q colocar o L
+        float altura = 1.75f;//float e mais rapidp q o double, ocupa menos espaço
+        byte quantidade = 10;//limitado melhor o int
+        short temperatura = -10; //msm do byte
+        //CONSTANTES
+        final double VALOR_PI = 3.14;//final normalente é declarada com letra maiuscula
+    }                                //Não pode ser mudado depois de declarado
+    
+}
