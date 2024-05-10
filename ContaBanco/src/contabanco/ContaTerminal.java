eba2b6f649bd4a5f3405f2ab4da5521942ebc9d6
Kaiky Pires <129606756+KaikyPires@users.noreply.github.com>
2024-05-09 21:03:51 -0300

    Update ContaTerminal.java

--- ContaBanco/src/contabanco/ContaTerminal.java
+++ ContaBanco/src/contabanco/ContaTerminal.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package contabanco;
 
-/**
- *
- * @author Kaiky Pires
- */
+import java.util.Scanner;
 public class ContaTerminal {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, digite o seu nome!");
+        String nomeDoUsuario = scanner.nextLine();
+        System.out.println("Por favor, digite o numero da conta!");
+        int numeroDaConta = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Por favor, digite o numero da Agência!");
+        String numeroDaAgencia = scanner.nextLine();
+        
+        double saldoDaConta = 237.48;
+        
+        
+        System.out.println("Olá " +nomeDoUsuario+ ", obrigado por criar uma conta em nosso banco,"
+                + " sua agência é " +numeroDaAgencia+ ", conta " +numeroDaConta+ " e seu " +saldoDaConta+ ""
+                        + " já está disponivel para saque");
+    }
     
 }
